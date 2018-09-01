8eedc70f4c41764077dd8e2ca47668cfd23e07c6
AbnerXiang <ios4zgx@gmail.com>
2018-09-01 19:22:19 +0800

    add vendor's survey

--- public/js/vendor.js
+++ public/js/vendor.js
@@ -1,38 +1,98 @@
 var vendors = [
-    {id: 1, vendor: "荣浦印务", contact: "高翔", email: "ios4zgx@gmail.com"},
-    {id: 2, vendor: "子龙印刷有限公司", contact: "张宇", email: "test@a.com"},
-    {id: 3, vendor: "浦佳科技有限公司", contact: "Abner", email: "12345678912@qq.com"},
-    {id: 1, vendor: "荣浦印务", contact: "高翔", email: "ios4zgx@gmail.com"},
-    {id: 2, vendor: "子龙印刷有限公司", contact: "张宇", email: "test@a.com"},
-    {id: 3, vendor: "浦佳科技有限公司", contact: "Abner", email: "12345678912@qq.com"},
-    {id: 1, vendor: "荣浦印务", contact: "高翔", email: "ios4zgx@gmail.com"},
-    {id: 2, vendor: "子龙印刷有限公司", contact: "张宇", email: "test@a.com"},
-    {id: 3, vendor: "浦佳科技有限公司", contact: "Abner", email: "12345678912@qq.com"},
-    {id: 1, vendor: "荣浦印务", contact: "高翔", email: "ios4zgx@gmail.com"},
-    {id: 2, vendor: "子龙印刷有限公司", contact: "张宇", email: "test@a.com"},
-    {id: 3, vendor: "浦佳科技有限公司", contact: "Abner", email: "12345678912@qq.com"},
-    {id: 1, vendor: "荣浦印务", contact: "高翔", email: "ios4zgx@gmail.com"},
-    {id: 2, vendor: "子龙印刷有限公司", contact: "张宇", email: "test@a.com"},
-    {id: 3, vendor: "浦佳科技有限公司", contact: "Abner", email: "12345678912@qq.com"},
-    {id: 1, vendor: "荣浦印务", contact: "高翔", email: "ios4zgx@gmail.com"},
-    {id: 2, vendor: "子龙印刷有限公司", contact: "张宇", email: "test@a.com"},
-    {id: 3, vendor: "浦佳科技有限公司", contact: "Abner", email: "12345678912@qq.com"},
-    {id: 1, vendor: "荣浦印务", contact: "高翔", email: "ios4zgx@gmail.com"},
-    {id: 2, vendor: "子龙印刷有限公司", contact: "张宇", email: "test@a.com"},
-    {id: 3, vendor: "浦佳科技有限公司", contact: "Abner", email: "12345678912@qq.com"},
-    {id: 1, vendor: "荣浦印务", contact: "高翔", email: "ios4zgx@gmail.com"},
-    {id: 2, vendor: "子龙印刷有限公司", contact: "张宇", email: "test@a.com"},
-    {id: 3, vendor: "浦佳科技有限公司", contact: "Abner", email: "12345678912@qq.com"},
+    { id: 1, vendor: "荣浦印务", contact: "高翔", email: "ios4zgx@gmail.com" },
+    { id: 2, vendor: "子龙印刷有限公司", contact: "张宇", email: "test@a.com" },
+    { id: 3, vendor: "浦佳科技有限公司", contact: "Abner", email: "12345678912@qq.com" },
+    { id: 1, vendor: "荣浦印务", contact: "高翔", email: "ios4zgx@gmail.com" },
+    { id: 2, vendor: "子龙印刷有限公司", contact: "张宇", email: "test@a.com" },
+    { id: 3, vendor: "浦佳科技有限公司", contact: "Abner", email: "12345678912@qq.com" },
+    { id: 1, vendor: "荣浦印务", contact: "高翔", email: "ios4zgx@gmail.com" },
+    { id: 2, vendor: "子龙印刷有限公司", contact: "张宇", email: "test@a.com" },
+    { id: 3, vendor: "浦佳科技有限公司", contact: "Abner", email: "12345678912@qq.com" },
+    { id: 1, vendor: "荣浦印务", contact: "高翔", email: "ios4zgx@gmail.com" },
+    { id: 2, vendor: "子龙印刷有限公司", contact: "张宇", email: "test@a.com" },
+    { id: 3, vendor: "浦佳科技有限公司", contact: "Abner", email: "12345678912@qq.com" },
+    { id: 1, vendor: "荣浦印务", contact: "高翔", email: "ios4zgx@gmail.com" },
+    { id: 2, vendor: "子龙印刷有限公司", contact: "张宇", email: "test@a.com" },
+    { id: 3, vendor: "浦佳科技有限公司", contact: "Abner", email: "12345678912@qq.com" },
+    { id: 1, vendor: "荣浦印务", contact: "高翔", email: "ios4zgx@gmail.com" },
+    { id: 2, vendor: "子龙印刷有限公司", contact: "张宇", email: "test@a.com" },
+    { id: 3, vendor: "浦佳科技有限公司", contact: "Abner", email: "12345678912@qq.com" },
+    { id: 1, vendor: "荣浦印务", contact: "高翔", email: "ios4zgx@gmail.com" },
+    { id: 2, vendor: "子龙印刷有限公司", contact: "张宇", email: "test@a.com" },
+    { id: 3, vendor: "浦佳科技有限公司", contact: "Abner", email: "12345678912@qq.com" },
+    { id: 1, vendor: "荣浦印务", contact: "高翔", email: "ios4zgx@gmail.com" },
+    { id: 2, vendor: "子龙印刷有限公司", contact: "张宇", email: "test@a.com" },
+    { id: 3, vendor: "浦佳科技有限公司", contact: "Abner", email: "12345678912@qq.com" },
 ];
+var survey = {
+    pages: [
+        {
+            name: "page1",
+            elements: [
+                {
+                    type: "text",
+                    name: "name",
+                    title: "Please enter your name:",
+                    isRequired: true,
+                    placeHolder: "Jon Snow"
+                },
+                {
+                    type: "text",
+                    name: "birthdate",
+                    title: "Your birthdate:",
+                    isRequired: true,
+                    inputType: "date"
+                },
+                {
+                    type: "text",
+                    name: "email",
+                    title: "Your e-mail:",
+                    isRequired: true,
+                    validators: [
+                        {
+                            type: "email"
+                        }
+                    ],
+                    inputType: "email",
+                    placeHolder: "jon.snow@nightwatch.org"
+                }
+            ]
+        },
+        {
+            name: "page2",
+            elements: [
+                {
+                    type: "checkbox",
+                    name: "question1",
+                    choices: [
+                        "item1",
+                        "item2",
+                        "item3"
+                    ]
+                },
+                {
+                    type: "file",
+                    name: "question3",
+                    maxSize: 0
+                },
+                {
+                    type: "expression",
+                    name: "question2"
+                }
+            ]
+        }
+    ]
+}
 
-$(function(){
+$(function () {
 
     var self = {
-        init(){
+        init() {
             this.addEvent();
         },
-        addEvent(){
+        addEvent() {
             this.loadTableData();
+            this.loadSurvey();
         },
         loadTableData() {
             var languageURL;
@@ -53,11 +113,15 @@ $(function(){
                     { data: "vendor" },
                     { data: "contact" },
                     { data: "email" },
-                ]
+                ],
             });
+        },
+        loadSurvey() {
+            window.survey = new Survey.Model(survey);
+            $("#surveyElement").Survey({ model: survey });
         }
     }
-    
+
     self.init();
 
 })
